5bc71461a05be53068ac4b5850453ba90f85be86
Vincent Tsang <yukun.zeng.hit@outlook.com>
2016-11-02 22:35:33 -0500

    initial commit, integrated princeton graph lib, added a sorted edge set for our algo

---
+++ src/com/lenss/yzeng/GreedyDivider.java
@@ -0,0 +1,18 @@
+package com.lenss.yzeng;
+
+import java.util.ArrayList;
+
+import edu.princeton.cs.algs4.Edge;
+import edu.princeton.cs.algs4.Quick;
+import edu.princeton.cs.algs4.QuickX;
+
+public class GreedyDivider {
+	public GreedyDivider() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public EdgeWeightedGraph[] greedyDivide(EdgeWeightedGraph graph){
+		//Quick.sort(a);
+		
+	}
+}
